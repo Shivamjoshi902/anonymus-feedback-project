22c28e59e6858d6a1eda0e9ce48044b0a02977a1
Shivamjoshi902 <sj8630470355@gmail.com>
2025-09-23 11:11:01 +0530

    added home navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,16 @@ import { User } from 'next-auth'
 import Link from 'next/link'
 import { Button } from './ui/button'
 
+
 const Navbar = () => {
     const {data : session} = useSession();
     const user : User = session?.user;
   return (
     <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-            <a href="#" className="text-xl font-bold mb-4 md:mb-0">
-            True Feedback
-            </a>
+            <Link href="/" className="text-xl font-bold mb-4 md:mb-0">
+                True Feedback
+            </Link>
             
             {session ? (
                 <>
